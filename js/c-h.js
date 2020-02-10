46309a3ad6df81adfd0852dbe601a263197cb10b
95mohahm <kurtular.vadisi@gmail.com>
2020-02-10 22:31:57 +0100

    search bar in place

---
+++ js/c-h.js
@@ -0,0 +1,16 @@
+
+window.onload = function(){
+  document.getElementById("filter-button").addEventListener("click",switchFilter)  
+};
+
+//show-hide filter div.
+function switchFilter(){
+    var filter = document.getElementById("filter");
+    if(filter.style.display == "none" || filter.style.display == "" ){
+        document.querySelector(".fa-caret-down").className="fas fa-caret-up";
+        filter.style.display = "block";
+    }else{
+         filter.style.display = "none";
+         document.querySelector(".fa-caret-up").className="fas fa-caret-down";
+        }
+    };
